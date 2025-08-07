891064413c8f9a6bd4f724a44415c0dec93ee7f3
Catwomen777 <raushawillis336@gmail.com>
2025-08-07 15:57:36 -0700

    first commit

---
+++ CWS.py
@@ -0,0 +1,18 @@
+def check_even_odd(number):
+
+ 
+    if number % 2 == 0:
+        return "Even"
+    else :
+        return "Odd"
+    
+def no_space(x):
+    
+    no_spaces = x.replace(" " ,"")
+    
+    return no_spaces
+
+def number_to_string(num):
+
+    s = str(num)
+    return s
